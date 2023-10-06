decaea41f5de5be82602c0a4eeccc5723c4b1155
Abiola Bamigboye <bamigboyeabiola@gmail.com>
2023-10-06 09:46:22 +0100

    Corrected errors in  app.ts

--- src/app.ts
+++ src/app.ts
@@ -21,7 +21,6 @@ app.get('/', (req, res) => res.send('Connected'));
 app.use('/api/images', imagesRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/projects', projectRouter);
-=======
 app.use('/api/hero', heroRouter);
 app.use('/api/news', newsRouter);
 
